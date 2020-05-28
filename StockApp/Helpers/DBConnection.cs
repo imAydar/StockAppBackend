06c918241f75ad17a9565760aec423c3e1d30a86
Aydaar <planeta_aydar@mail.ru>
2020-05-28 16:04:50 +0500

    now doesnt require to make changes in 1c

--- StockApp/Helpers/DBConnection.cs
+++ StockApp/Helpers/DBConnection.cs
@@ -12,49 +12,50 @@ namespace StockApp.Helpers
 {
     public static class DBConnection
     {
+        private static dynamic connection1c = null;
 
-        public static dynamic connection1c;
         static DBConnection()
         {
-            if (connection1c == null)
-            {
-                var connector = new COMConnector();
-                var settings = ConfigurationManager.ConnectionStrings
-                                               .Cast<ConnectionStringSettings>()
-                                               .Where(c => c.Name == "V83").FirstOrDefault();
-                String connectStr = settings.ConnectionString;
-                connector.PoolCapacity = 10;
-                connector.PoolTimeout = 60;
-                connector.MaxConnections = 12;
-                connection1c = connector.Connect(connectStr);
-            }
+                Connect();
         }
 
+        private static void Connect()
+        {
+
+            var connector = new COMConnector();
+            var settings = ConfigurationManager.ConnectionStrings
+                                           .Cast<ConnectionStringSettings>()
+                                           .Where(c => c.Name == "V83").FirstOrDefault();
+            String connectStr = settings.ConnectionString;
+            connector.PoolCapacity = 10;
+            connector.PoolTimeout = 60;
+            connector.MaxConnections = 12;
+            connection1c = connector.Connect(connectStr);
+        }
 
         public static List<Document> GetLastDocuments(int amount = 20)
         {
-                dynamic dataArray1C = connection1c.Документы.ИнвентаризацияТоваровНаСкладе.Выбрать(null, null, null, "Дата убыв");
-                var documents = new List<Document>();
-                int ind = 0;
-                while (dataArray1C.Следующий() == true)
+            var documents = new List<Document>();
+            dynamic dataArray1C = connection1c.Документы.ИнвентаризацияТоваровНаСкладе.Выбрать(null, null, null, "Дата убыв");
+
+            int ind = 0;
+            while (dataArray1C.Следующий() == true)
+            {
+                documents.Add(new Document()
                 {
-                    documents.Add(new Document()
-                    {
-                        Number = dataArray1C.Номер,
-                        Date = dataArray1C.Дата,
-                        Owner = dataArray1C.Ответственный.Наименование,
-                        Comment = dataArray1C.Комментарий
-                    });
-                    ind++;
-                    if (ind >= amount) break;
-                }
-                return documents;
-            
+                    Number = dataArray1C.Номер,
+                    Date = dataArray1C.Дата,
+                    Owner = dataArray1C.Ответственный.Наименование,
+                    Comment = dataArray1C.Комментарий
+                });
+                ind++;
+                if (ind >= amount) break;
+            }
+            return documents;
         }
 
         public static IEnumerable<DocumentRow> GetRows(string DocumentCode, DateTime DocumentDate)
         {
-            
             dynamic comDocument = connection1c.Документы.ИнвентаризацияТоваровНаСкладе.НайтиПоНомеру(DocumentCode, DocumentDate);
             foreach (var row in comDocument.Товары)
             {
@@ -68,16 +69,63 @@ namespace StockApp.Helpers
             }
         }
 
-        public static string AddRow(DocumentRow row)
+        public static string AddRow(DocumentRow Row)
         {
             dynamic docs = connection1c.Документы.ИнвентаризацияТоваровНаСкладе;
-            dynamic comDocument = docs.НайтиПоНомеру(row.Document.Number, row.Document.Date);
-
+            dynamic comDocument = docs.НайтиПоНомеру(Row.Document.Number, Row.Document.Date);
             dynamic uid = connection1c.NewObject("УникальныйИдентификатор", (comDocument.УникальныйИдентификатор()));
             dynamic comDocJbj = docs.ПолучитьСсылку(uid).ПолучитьОбъект();
 
-            return comDocJbj.ДобавитьШтрихкод(row.Barcode);
+            dynamic ware = GetWare(Row.Barcode);
+            if (ware == null)
+                return "Ошибка: не была найдена номенклатура";
+
+            if (AddRow(ware))
+                return ware.Код;
+
+            return "Ошибка: не удалось добавить номенклатуру";
         }
- 
+
+        private static dynamic GetWare(string Barcode)
+        {
+            dynamic query = connection1c.NewObject("Запрос");
+            query.Текст = @"ВЫБРАТЬ
+	            Штрихкоды.Владелец как ссылка
+            ИЗ
+	            РегистрСведений.Штрихкоды КАК Штрихкоды
+            ГДЕ
+	            Штрихкоды.Штрихкод = &Штрихкод";
+            query.УстановитьПараметр("Штрихкод", Barcode);
+            dynamic req = query.Выполнить().Выбрать();
+            while (req.Следующий())
+            {
+                return req.Ссылка;
+            }
+            return null;
+        }
+
+        private static Boolean AddRow(dynamic Ware, dynamic Doc)
+        {
+            dynamic str = Doc.Товары.Найти(Ware, "Номенклатура");
+            if (str == null)
+            {
+                str = Doc.Товары.Добавить();
+                str.Номенклатура = Ware;
+                str.Количество = 0;
+                str.Коэффициент = 1;
+                str.ЕдиницаИзмерения = Ware.ЕдиницаХраненияОстатков;
+            }
+            str.Количество += 1;
+            try
+            {
+                Doc.Записать();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
     }
 }
